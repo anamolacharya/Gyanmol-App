48813cfbf0aa3e51e3e73532302f508359a84a36
anamolacharya <anamol222@gmailcom>
2021-07-14 22:04:40 -0500

    Added footer in Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
+import Footer from "../components/Footer";
 
 const HomePage = () => (
-    <>
-<h1>Hello,Welcome to my Website!</h1>
+  <>
+    <h1>Hello,Welcome to my Website!</h1>
 
-<p>
-Hello Everyone, Welcome to my Website! I am gradually updating this site in coming days.  
-I find myself learning new skills and programming languages, problem-solving, 
-responsive design principles, website optimization, software engineering, and Model View Controller (MVC) methods of organizing code. 
-I have experience using Java, Javascript, Azure Fundamental, Node.js, ReactJS, and I've started diving into lambda functions on AWS and RESTful APIs. 
-I am enthusiastically open to learn new programming languages, frameworks, and libraries and work on positions that get me involved in any creative and technical areas. 
-</p>
-</>
-    
+    <p>
+      Hello Everyone, Welcome to my Website! I am gradually updating this site
+      in coming days. I find myself learning new skills and programming
+      languages, problem-solving, responsive design principles, website
+      optimization, software engineering, and Model View Controller (MVC)
+      methods of organizing code. I have experience using Java, Javascript,
+      Azure Fundamental, Node.js, ReactJS, and I've started diving into lambda
+      functions on AWS and RESTful APIs. I am enthusiastically open to learn new
+      programming languages, frameworks, and libraries and work on positions
+      that get me involved in any creative and technical areas.
+    </p>
+    <Footer />
+  </>
 );
 
 export default HomePage;
