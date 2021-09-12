8aaf3a35d9845456c50e0be2e379de0efebe9944
anamolacharya <anamol222@gmailcom>
2021-09-12 00:29:22 -0500

    Added practise set 1 demo

--- src/pages/TestPrepPage.js
+++ src/pages/TestPrepPage.js
@@ -1,7 +1,123 @@
-import React from 'react';
+import React from "react";
 
 const TestPrepPage = () => (
-
+  <>
     <h1>Welcome to Test Preparation page!</h1>
+    <div>
+      {/* 1,2,3,4,5,6,14,16,18,20 */}
+      <h4>AWS Practice Set 1</h4>
+      <br />
+      <li>
+        Which AWS service provides a simple way to set up a new multi-account
+        AWS environment and govern it at scale?{" "}
+      </li>
+      <ul>A. AWS Trusted Advisor</ul>
+      <ul>B. AWS Security Hub</ul>
+      <ul>C. AWS Control Tower</ul>
+      <ul>D. AWS Resources Access Manager</ul>
+
+      <br />
+      <li>
+        {" "}
+        A user has underlined on-premises resources. Which AWS Cloud concept can
+        BEST address this issue?
+      </li>
+      <ul>A. High Availability</ul>
+      <ul>B. Elasticity</ul>
+      <ul>C. Security</ul>
+      <ul>D. Loose coupling</ul>
+
+      <br />
+      <li>
+        Which of the following are benefits of using the AWS CLoud? (Select
+        TWO.)
+      </li>
+      <ul>A. 100% fault tolerance</ul>
+      <ul>B. Total control over underlying infrastructure</ul>
+      <ul>C. Fast provisioning of IT resources</ul>
+      <ul>D. Outsourcing all application coding in AWS</ul>
+      <ul>E. Ability to go global quickly</ul>
+
+      <br />
+      <li>
+        A company has multiple data sources across the organization and wants to
+        consolidate data into one data warehouse. Which AWS service can be used
+        to meet this requirement?
+      </li>
+      <ul>A. Amazon DynamoDB</ul>
+      <ul>B. Amazon Redshift</ul>
+      <ul>C. Amazon Athena</ul>
+      <ul>D. Amazon QuickSight</ul>
+
+      <br />
+      <li>
+        A user with an AWS Basic Support plan has determined that illegal
+        activities are being run on their AWS resources. What is the recommended
+        method for the user to report the activity to AWS?
+      </li>
+      <ul>A. Contact the AWS Concierge Support Team</ul>
+      <ul>B. Contact an AWS technical account manager</ul>
+      <ul>C. Contact the AWS Abuse team</ul>
+      <ul>D. Contact the AWS Support team</ul>
+
+      <br />
+      <li>
+        A developer has been hired by a large company and needs AWS credentials.
+        Which are security best practices that should be followed? (Select TWO.)
+      </li>
+      <ul>
+        A. Grant the developer access to only the AWS resources needed to
+        perform the job.
+      </ul>
+      <ul>B. Share the AW account root user credentials with the developer.</ul>
+      <ul>C. Add the developer to the administrator’s group in AWS IAM.</ul>
+      <ul>
+        D. Configure a password policy that ensures the developer’s password
+        cannot be changed.
+      </ul>
+      <ul>E. Ensure the account password policy requires a minimum length.</ul>
+
+      <br />
+      <li>
+        Which credential allows programmatic access to AWS resources for use
+        from the AWS CLI or the AWS API?
+      </li>
+      <ul>A. User name and password</ul>
+      <ul>B. Access Keys</ul>
+      <ul>C. SSH public keys</ul>
+      <ul>D. AWS Key Management Services (AWS KMS) keys</ul>
+
+      <br />
+      <li>
+        Which AWS service provides a customized view of the health of specific
+        AWS services that power a customer’s workloads running on AWS?
+      </li>
+      <ul>A. AWS Service Health Dashboard</ul>
+      <ul>B. AWS X-Ray</ul>
+      <ul>C. AWS Personal Health Dashboard</ul>
+      <ul>D. Amazon CloudWatch</ul>
+
+      <br />
+      <li>
+        Which of the following enables users to leverag the power of AWS
+        SERVICES programmatically?
+      </li>
+      <ul>A. AWS Command Line Interface ( AWS CLI)</ul>
+      <ul>B. AWS Trusted Advisor</ul>
+      <ul>C. AWS CodeDeploy </ul>
+      <ul>D. AWS Management Console</ul>
+
+      <br />
+      <li>
+        What does the AWS Cloud provide to increase the speed and agility of
+        execution for customers? (Select two){" "}
+      </li>
+      <ul>A. Readily available resources low provisioning</ul>
+      <ul>B. Scalable compute capacity</ul>
+      <ul>C. Free Tier services usage</ul>
+      <ul>D. Access to AWS data centers</ul>
+      <ul>E. Lower resource provisioning cost</ul>
+    </div>
+  </>
 );
 export default TestPrepPage;
