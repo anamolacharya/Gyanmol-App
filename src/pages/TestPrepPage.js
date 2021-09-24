639ecd394ce89d1c9c7dd473b9041522d0b6aae8
anamolacharya <anamol222@gmailcom>
2021-09-24 03:03:33 -0500

    Added NCLEX Sample set in test prep

--- src/pages/TestPrepPage.js
+++ src/pages/TestPrepPage.js
@@ -20,7 +20,7 @@ const TestPrepPage = () => (
 
     <div>
       {/* 1,2,3,4,5,6,14,16,18,20 */}
-      <h4>AWS Practice Set 1</h4>
+      <h4>AWS-CCP Practice Set Sample</h4>
       <br />
       <li>
         Which AWS service provides a simple way to set up a new multi-account
@@ -132,7 +132,151 @@ const TestPrepPage = () => (
       <ul>C. Free Tier services usage</ul>
       <ul>D. Access to AWS data centers</ul>
       <ul>E. Lower resource provisioning cost</ul>
+      <br />
+      <br />
+    </div>
+
+    <div>
+      <h4>NCLEX Practise Test Sample</h4>
+      <br />
+      <li>
+        A client had a 10( - bag of 5% dextrose in 0.9 % sodium chloride hung at
+        3 . The nurse making rounds at 3/45 * P * M finds that the client is
+        complaining of a pounding headache and is dyspneic , is experiencing
+        chills , and is apprehensive , with an increased pulse rate . The
+        intravenous (IV ) bag has 400 remaining . The nurse should take which
+        action first?
+      </li>
+      <ul>1. Slow the IV infusion. </ul>
+      <ul>2. Sit the client up in bed.</ul>
+      <ul>3. Remove the IV catheter.</ul>
+      <ul>4. Call the health care provider (HCP) .</ul>
+
+      <br />
+      <li>
+        The nurse has a prescription to hang a 1000 - intravenous (IV ) bag of
+        5% dextrose in water with 20 mEq of potassium chloride and needs to add
+        the medication to the IV bag . The nurse should plan to take which
+        action immediately after injecting the potassium chloride into the port
+        of the IV bag ?
+      </li>
+      <ul>1. Rotate the bag gently. </ul>
+      <ul>2. Attach the tubing to the client.</ul>
+      <ul>3. Prime the tubing with the IV solution. </ul>
+      <ul>4. Check the solution for yellowish discoloration.</ul>
+
+      <br />
+      <li>
+        The nurse is completing a time tape for a 1000 - IV bag that is
+        scheduled to infuse over 8 hours . The nurse has just placed the 11:0(
+        marking at the 500 - level. The nurse would place the mark for noon at
+        which numerical level ( ) on the time tape ? Fill in the blank .
+      </li>
+      <ul>Answer: _____ ml.</ul>
+
+      <br />
+      <li>
+        The nurse is making initial rounds on the nursing unit to assess the
+        condition of assigned clients . The nurse notes that a client's
+        intravenous (IV ) site is cool , pale , and swollen , and the solution
+        is not infusing . The nurse concludes that which compli cation has
+        occurred?
+      </li>
+      <ul>1. Infection</ul>
+      <ul>2. Phlebitis </ul>
+      <ul>3. Infiltration </ul>
+      <ul>4. Thrombosis </ul>
+
+      <br />
+      <li>
+        The nurse is inserting an intravenous line into a client's vein . After
+        the initial stick , the nurse would continue to advance the catheter in
+        which situation?
+      </li>
+      <ul>1. The catheter advances easily.</ul>
+      <ul>2. The vein is distended under the needle. </ul>
+      <ul>3. The client does not complain of discomfort. </ul>
+      <ul>4. Blood return shows in the backflash chamber of the catheter.</ul>
+
+      <br />
+      <li>
+        The nurse notes that the site of a client's peripheral intravenous (IV )
+        catheter is reddened , warm , painful , and slightly edematous proximal
+        to the insertion point of the IV catheter . After taking appropriate
+        steps to care for the client , the nurse should document in the medical
+        record that the client experienced which condition?{" "}
+      </li>
+      <ul>1. Phlebitis of the vein</ul>
+      <ul>2. Infiltration of the IV line</ul>
+      <ul>3. Hypersensitivity to the IV solution</ul>
+      <ul>4. Allergic reaction to the IV catheter material</ul>
+
+      <br />
+      <li>
+        The nurse is preparing a continuous intravenous ( IV ) infusion at the
+        medication cart . As the nurse goes to insert the spike end of the IV
+        tubing into the IV bag , the tubing drops and the spike end hits the top
+        of the medication cart . The nurse should take which action ?
+      </li>
+      <ul>1. Obtain a new IV bag .</ul>
+      <ul>2. Obtain new IV tubing . </ul>
+      <ul>3. Wipe the spike end of the tubing with Betadine .</ul>
+      <ul>4. Scrub the spike end of the tubing with an alcohol swab.</ul>
+
+      <br />
+      <li>
+        A health care provider has written a prescription to discontinue an
+        intravenous (IV) line . The nurse should obtain which item from the unit
+        supply area for applying pressure to the site after removing the IV
+        catheter ?
+      </li>
+      <ul>1. Elastic wrap</ul>
+      <ul>2. Betadine swab </ul>
+      <ul>3. Adhesive bandage</ul>
+      <ul>4. Sterile 2x2 gauze</ul>
+
+      <br />
+      <li>
+        A client rings the call bell and complains of paint the nurse assesses
+        the site of an intravenous (IV) infusion. The nurse assesses the site
+        and determines that phlebitis has developed. The nurse should take which
+        action( s) in the care of this client ? Select all that apply .
+      </li>
+      <ul>1. Notify the health care provider ( ICP).</ul>
+      <ul>2. Remove the IV catheter at that site.</ul>
+      <ul>3. Apply warm moist packs to the site. </ul>
+      <ul>4. Start a new IV line in a proximal portion of the same vein.</ul>
+      <ul>
+        5. Document the occurrence , actions taken , and the client's response.
+      </ul>
+
+      <br />
+      <li>
+        A client involved in a motor vehicle crash presents to the emergency
+        department with severe internal bleeding . The client is severely
+        hypotensive and unresponsive . The nurse anticipates that which
+        intravenous (IV ) solution will most likely be prescribed to increase
+        intravascular volume , replace immediate blood loss volume , and
+        increase blood pressure ?{" "}
+      </li>
+      <ul>1. 5 % dextrose in lactated Ringer's</ul>
+      <ul>2. 0.33% sodium chloride (1/3 normal saline )</ul>
+      <ul>3. 0.225% sodium chloride (1/4 normal saline)</ul>
+      <ul>4. 0.45% sodium chloride (1/2 normal saline)</ul>
     </div>
   </>
+  /*
+Answers
+1. 1
+2. 1
+3. 375ml
+4. 3
+5. 4
+6. 1
+7. 2
+8. 4
+9. 1,2,3,5
+10. 1
+*/
 );
 export default TestPrepPage;
