2926167b94049d73a648857f13e6132fa1cb1f79
anamolacharya <anamol222@gmailcom>
2021-09-29 00:41:18 -0500

    added sontent for home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,12 @@ import Footer from "../components/Footer";
 
 const HomePage = () => (
   <>
-    <h1>Hello,Welcome to Gyanmol!</h1>
+    <h1>Welcome to Gyanmol!</h1>
+    <h3>Carosel </h3>
+    <h3>Search bar for searching courses/tests</h3>
+    <h3>Content section 1</h3>
+    <h3>Content section 2</h3>
+    
   </>
 );
 
